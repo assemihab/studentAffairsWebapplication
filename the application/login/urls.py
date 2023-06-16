eb4f1e6a622417d2fb7548d35693b6fc35cf55a9
Assem Ihab <87605812+assemihab@users.noreply.github.com>
2023-06-16 10:07:51 +0300

    Update urls.py

--- the application/login/urls.py
+++ the application/login/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'login'
 ##########dont forget to chage the javascript to dynamicccccc###############################
 urlpatterns = [path('', views.index, name='index'),
+               path('submit/userprofile.html', views.userProfie, name='userProfile'),
                path('homepage/', views.homepage, name='homepage'),
                path('login/', views.loginpage, name='log'),
                path('submit/',views.submit,name='submission'),
@@ -13,5 +14,9 @@ urlpatterns = [path('', views.index, name='index'),
                path('addnewstudent/',views.addnewStud,name="addnew"),
                path('add-student/', views.addStudent, name='add_student'),
                path('updateStudent.html', views.update_student, name='update_student'),
+               path('edit_student/',views.editstud,name="editstudent"),
+               path('department-form.html', views.editDept, name='editDepartment'),
+               path('assignDept/',views.assigndept,name="assigndepartmentbut"),
+               path('change_toact/',views.changestatus,name="changetoactt"),
             #    path('department-form.html', views.department_form, name='department_form'),
                 ]
